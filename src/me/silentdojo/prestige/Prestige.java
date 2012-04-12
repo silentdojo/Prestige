05e044f0fa0a683450c14df2679a1b4b6fd265c8
Bryan Mendish <silentdojo@incraftion.com>
2012-04-12 04:28:46 -0400

    Ummm yea

--- src/me/silentdojo/prestige/Prestige.java
+++ src/me/silentdojo/prestige/Prestige.java
@@ -45,7 +45,7 @@ public class Prestige extends JavaPlugin {
 	    		sender.sendMessage(ChatColor.AQUA + ChatColor.UNDERLINE + "Prestige is a new way to advance your skills.");
 	    		sender.sendMessage("| " + ChatColor.AQUA + "Gain amazing new abilities by focusing on specific Skills.");
 	    		sender.sendMessage("| " + ChatColor.AQUA + "Choose wisely. Benefits come at a price!");
-	 			sender.sendMessage("| " + ChatColor.AQUA + "Each Prestige resets your Skill Level.");
+	    		sender.sendMessage("| " + ChatColor.AQUA + "Each Prestige resets your Skill Level.");
 	    		sender.sendMessage("| " + ChatColor.AQUA + "PvP Skill Prestige will remove a PvE Ability.");
 	    		sender.sendMessage("| " + ChatColor.AQUA + "PvE Skill Prestige will remove a PvP Ability.");
 	    		sender.sendMessage("| " + ChatColor.DARK_RED + "Use " + ChatColor.ITALIC + "/prestige <Skillname>");
